84b95a08786f0216cc529dfbb753915745c65812
goliatone <burgosemi@gmail.com>
2015-03-02 15:10:51 -0500

    JS: Added beacons.helper

---
+++ public/js/app/utils/beacons.helper.js
@@ -0,0 +1,71 @@
+define('beacons.helper', function(require){
+
+    function getPosition(beacons, geometry){
+        if(!beacons) return console.warn('getPosition: No beacons');
+        beacons = beacons.sort(function(a, b){
+            return a.distance - b.distance;
+        });
+
+        var set = beacons.map(function(beacon){
+            var point = {};
+            point.id = 'UPDATE_THIS_REALLY!!',
+            point.r = beacon.rssi;
+            point.d = Math.round(beacon.distance * 100);
+            point.x = geometry[beacon.major + '::' + beacon.minor].x;
+            point.y = geometry[beacon.major + '::' + beacon.minor].y;
+            return point;
+        });
+
+        // return distancePoints(set)[0];
+
+        set = set.slice(0, 3);
+        return set.concat([getCoordinate.apply(null, set)]);
+        // return getTrilateration.apply(null, set);
+        return getCoordinate.apply(null, set);
+    }
+
+    function debugGetCoordinate(p1, p2, p3){
+        return [p1, p2, p3, getTrilateration(p1, p2, p3)];
+    }
+
+    function getCoordinate(p1, p2, p3) {
+        var w = p1.d * p1.d - p2.d * p2.d - p1.x * p1.x - p1.y * p1.y + p2.x * p2.x + p2.y * p2.y;
+
+        var z = p2.d * p2.d - p3.d * p3.d - p2.x * p2.x - p2.y * p2.y + p3.x * p3.x + p3.y * p3.y;
+
+        var x = (w * (p3.y - p2.y) - z * (p2.y - p1.y)) / (2 * ((p2.x - p1.x) * (p3.y - p2.y) - (p3.x - p2.x) * (p2.y - p1.y)));
+
+        var y = (w - 2 * x * (p2.x - p1.x)) / (2 * (p2.y - p1.y));
+
+        var v = (z - 2 * x * (p3.x - p2.x)) / (2 * (p3.y - p2.y));
+        y = (y + v) / 2;
+
+        return {
+            x: Math.round(x),
+            y: Math.round(y),
+            c:'#ff3366'
+        };
+    }
+
+    function getTrilateration(pa, pb, pc) {
+        var S = (Math.pow(pc.x, 2.) - Math.pow(pb.x, 2.) + Math.pow(pc.y, 2.) - Math.pow(pb.y, 2.) + Math.pow(pb.r, 2.) - Math.pow(pc.r, 2.)) / 2.0;
+        var T = (Math.pow(pa.x, 2.) - Math.pow(pb.x, 2.) + Math.pow(pa.y, 2.) - Math.pow(pb.y, 2.) + Math.pow(pb.r, 2.) - Math.pow(pa.r, 2.)) / 2.0;
+        var y = ((T * (pb.x - pc.x)) - (S * (pb.x - pa.x))) / (((pa.y - pb.y) * (pb.x - pb.x)) - ((pc.y - pb.y) * (pb.x - pa.x)));
+        var x = ((y * (pa.y - pb.y)) - T) / (pb.x - pa.x);
+        console.log('TRILATERATION', {x:x, y:y})
+        return {
+            x: x,
+            y: y,
+            c:'#ff3366'
+        };
+    }
+
+    window.getCoordinate = getCoordinate;
+    window.debugGetCoordinate = debugGetCoordinate;
+
+    return {
+        getPosition: getPosition,
+        getCoordinate: getCoordinate,
+        debugGetCoordinate: debugGetCoordinate,
+    }
+});
