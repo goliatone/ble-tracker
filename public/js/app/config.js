3d988c7379fee122ae3e0347f6073b3949eac5ca
goliatone <burgosemi@gmail.com>
2015-03-02 15:11:37 -0500

    APP: Added config file

---
+++ public/js/app/config.js
@@ -0,0 +1,14 @@
+define('config', function(require){
+    // var GConfig = require('gconfig');
+
+    return {
+        geometry: {
+            "333::1": {"x":2074, "y":685, "label": "middle-right"},
+            "333::2": {"x":1664, "y":51, "label": "top-left"},
+            "333::3": {"x":1675, "y":685, "label": "middle-left"},
+            "444::1": {"x":1675, "y":1307, "label": "bottom-left"},
+            "444::2": {"x":2148, "y":1307, "label": "bottom-right"},
+            "444::3": {"x":2125, "y":51, "label": "top-right"}
+        }
+    }
+});
