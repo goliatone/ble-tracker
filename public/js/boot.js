08913c5c734613c95cbdd4668c437dccdd22b39c
goliatone <burgosemi@gmail.com>
2015-03-03 16:19:46 -0500

    JS: boot add ractive and plugins for require

--- public/js/boot.js
+++ public/js/boot.js
@@ -3,9 +3,11 @@
 requirejs.config({
     baseUrl: '/js',
     paths: {
-        'main': 'app/main',
-        'config': 'app/config',
+        main: 'app/main',
+        config: 'app/config',
+
         floormap:'app/widgets/floormap',
+        userside: 'app/widgets/userside/userside',
 
         socket:'app/services/socket',
         'beacons.helper': 'app/utils/beacons.helper',
@@ -15,31 +17,47 @@ requirejs.config({
         floorplan:'vendors/floorplan/d3.floorplan.min',
         scatterplot:'app/utils/d3.floorplan.scatterplot',
 
-        'gpub': 'vendors/gpub/src/gpub',
+        gpub: 'vendors/gpub/src/gpub',
 
         // 'gconfig': 'vendors/gconfig/gconfig',
         // 'gconfig.path': 'vendors/gconfig/gconfig.path',
         // 'gconfig.qstring': 'vendors/gconfig/gconfig.qstring',
         // 'gconfig.interpolate': 'vendors/gconfig/gconfig.interpolate',
 
-        'extend': 'vendors/gextend/src/extend',
+        extend: 'vendors/gextend/src/extend',
         // 'keypath': 'vendors/gkeypath/keypath',
         // 'templatecontext': 'vendors/templatecontext/templatecontext',
 
 
 
+        'text': 'vendors/requirejs-text/text',
         // 'preloader': 'views/preloader',
-        // 'ractive': 'vendors/ractive/ractive',
+        'ractive': 'vendors/ractive/ractive',
         // 'jquery': 'vendors/jquery/jquery',
-
+    },
+    map: {
+        '*': {
+            'css': 'vendors/require-css/css'
+        }
     }
 });
+
 define('boot', function(require) {
     console.warn('Loading');
 
     require('main');
     require('floormap');
 
+    require('userside');
+
+    var Ractive = require('ractive');
     // var App = require('app');
+    //
+    var view = new Ractive({
+        template: '#content-template',
+        el: 'content',
+        append: true,
+        message:'Hola Mundo Mundial!!!'
+    });
 
 });
