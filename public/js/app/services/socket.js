3029cbd7603e8158f3de42d349d45fca2a22bc91
goliatone <burgosemi@gmail.com>
2015-03-04 16:35:18 -0500

    JS: App socket service generated uuid

--- public/js/app/services/socket.js
+++ public/js/app/services/socket.js
@@ -1,6 +1,6 @@
 define('socket', function(require){
     var io = require('socketio');
-    var gpub = require('gpub');
+    var uuid = require('uuid');
     var extend = require('extend');
 
     var DEFAULTS = {
@@ -9,7 +9,7 @@ define('socket', function(require){
             url: window.location.protocol + '//' + window.location.host
         },
         ackPayload: {
-            uuid:getUUID()
+            uuid: uuid.getUUID()
         },
         WSClient: function(config){
             return io(config.url);
@@ -43,23 +43,7 @@ define('socket', function(require){
         console.log('ON DISCONNECT', arguments);
     };
 
+
+
     return Client;
 });
-
-
-
-function getUUID() {
-    function makeAndStore() {
-        var uuid = makeUUID();
-        localStorage.setItem('__uuid__', uuid);
-        return uuid;
-    }
-
-    function makeUUID() {
-        return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(a, b) {
-            return b = Math.random() * 16, (a == "y" ? b & 3 | 8 : b | 0).toString(16)
-        });
-    }
-
-    return localStorage.getItem('__uuid__') || makeAndStore();
-}
