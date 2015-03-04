c2c91405f8e82c41e69fa732c3f9e35d45ec6264
goliatone <burgosemi@gmail.com>
2015-03-04 16:38:01 -0500

    BOOT: moving start logic here for now, WIP

--- public/js/boot.js
+++ public/js/boot.js
@@ -4,7 +4,9 @@ requirejs.config({
     baseUrl: '/js',
     paths: {
         main: 'app/main',
+        app: 'app/app',
         config: 'app/config',
+        uuid:'app/utils/uuid',
 
         floormap:'app/widgets/floormap',
         userside: 'app/widgets/userside/userside',
@@ -33,8 +35,9 @@ requirejs.config({
         'text': 'vendors/requirejs-text/text',
         // 'preloader': 'views/preloader',
         'ractive': 'vendors/ractive/ractive',
-        // 'jquery': 'vendors/jquery/jquery',
+        'jquery': 'vendors/jquery/dist/jquery',
     },
+
     map: {
         '*': {
             'css': 'vendors/require-css/css'
@@ -45,19 +48,62 @@ requirejs.config({
 define('boot', function(require) {
     console.warn('Loading');
 
-    require('main');
+    require('jquery');
+    // require('main');
     require('floormap');
-
     require('userside');
 
-    var Ractive = require('ractive');
-    // var App = require('app');
     //
-    var view = new Ractive({
+    var GPub = require('gpub');
+    var Client = require('socket');
+    var BeaconsHelper = require('beacons.helper');
+
+    var geometry = require('config').geometry;
+
+    GPub.observable(Client);
+
+    var Ractive = require('ractive');
+    var App = require('app');
+    var view;
+    var socket;
+
+    socket = new Client({});
+
+    socket.client.on('ble.inrange', function renderPosition(payload) {
+        var pos = BeaconsHelper.getPosition(payload.beacons, geometry);
+        pos ? pos.id = payload.uuid : (pos = {});
+        if(isNaN(pos.x) || isNaN(pos.y) || Math.abs(pos.x) === Infinity || Math.abs(pos.y) === Infinity ) return console.warn('OUT', pos);
+        pl.refresh([pos], function(d){ return d.id; });
+    });
+
+    socket.client.on('ble.inrange', function addUser(payload){
+        //TODO: we need to mange this better
+        var member = {
+            id: payload.uuid
+        };
+
+        view.findComponent('user-side').merge('members', [member], {
+            compare: 'id'
+        });
+    });
+
+    socket.client.on('device.connected', function(data) {
+        console.log('NEW DEVICE CONNECTED', data);
+    });
+
+    socket.client.on('device.disconnected', function(data) {
+        console.log('NEW DEVICE CONNECTED', data);
+    });
+    //
+
+
+    view = new Ractive({
         template: '#content-template',
         el: 'content',
         append: true,
         message:'Hola Mundo Mundial!!!'
     });
+
+    window.v = view;
 
 });
