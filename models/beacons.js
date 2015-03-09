563f71c448be80af4cd512fa5f982354762a53a0
goliatone <burgosemi@gmail.com>
2015-03-09 17:51:19 -0400

    MODEL: dirty beacon...

---
+++ models/beacons.js
@@ -0,0 +1,20 @@
+var path = require('path');
+var Engine = require('tingodb')();
+
+var db = new Engine.Db(path.resolve('./data'), {});
+
+function Beacon(){}
+Beacon.pings = db.collection('beacon_pings');
+Beacon.confs = db.collection('beacon_configurations');
+
+
+Beacon.savePing = function(payload){
+    Beacon.pings.insert([payload]);
+};
+
+Beacon.saveConfig = function(payload){
+    Beacon.confs.insert([payload]);
+};
+
+
+module.exports = Beacon;
