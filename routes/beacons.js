a505e7917032858036cb084fd8bf3e549f167f42
goliatone <burgosemi@gmail.com>
2015-03-11 13:33:55 -0400

    ROUTES: POC beacon tracker/config

--- routes/beacons.js
+++ routes/beacons.js
@@ -1,4 +1,5 @@
 var express = require('express'),
+    extend = require('gextend'),
     router = express();
 
 var Beacon = require('../models/beacons');
@@ -7,9 +8,47 @@ router.get('/beacon', function(req, res) {
     res.sendFile(process.env.PWD + '/public/beacon.html');
 });
 
+
+router.get('/uuid/:uuid/:major/:minor', function(req, res){
+    var query = {
+        uuid: req.params.uuid,
+        major: req.params.major,
+        minor: req.params.minor,
+        // location: settings.store_id
+    };
+
+    console.log('GET', query);
+
+    Beacon.beacons.find(query).toArray(function(err, docs) {
+        res.json({
+            success: true,
+            results: docs
+        });
+    });
+});
+
+router.post('/uuid/:uuid/:major/:minor/', function(req, res) {
+    var payload =  req.body;
+    // var payload =  extend({}, req.body);
+
+    payload.uuid = req.params.uuid;
+    payload.major = req.params.major;
+    payload.minor = req.params.minor;
+
+    payload.created_at = Date.now();
+
+    Beacon.beacons.insert([payload]);
+
+    res.json({
+        success: true,
+        beacon: payload
+    });
+});
+
 //PULL FROM ROUTES
-// app.get('/uuid/:beacon/:major/:minor/', function(req, res){
-//   res.sendfile('templates/beacon.html');
+// app.get('/uuid/:uuid/:major/:minor/', function(req, res){
+//     Beacon.beacons.find()
+//     res.send('templates/beacon.html');
 // });
 
 // app.post('/uuid/:beacon/:major/:minor/', function(req, res){
