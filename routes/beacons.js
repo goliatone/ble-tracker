1902b356da5cf89271853a51598d3ccd3d2823e8
goliatone <burgosemi@gmail.com>
2015-03-12 17:58:53 -0400

    ROUTES: clean beacon.js

--- routes/beacons.js
+++ routes/beacons.js
@@ -43,64 +43,6 @@ router.post('/uuid/:uuid/:major/:minor/', function(req, res) {
     });
 });
 
-//PULL FROM ROUTES
-// app.get('/uuid/:uuid/:major/:minor/', function(req, res){
-//     Beacon.beacons.find()
-//     res.send('templates/beacon.html');
-// });
-
-// app.post('/uuid/:beacon/:major/:minor/', function(req, res){
-//     var filePath = __dirname + '/public/' + req.params.beacon + '.json';
-//     fs.writeFile(filePath, req.body.data, function () {
-//         res.redirect('/data/' + req.params.beacon + '/' + req.params.major + '/' + req.params.minor + '/');
-//     });
-// });
-
-// app.get('/data/:beacon/:major/:minor/', function(req, res){
-//   var filePath = __dirname + '/public/' + req.params.beacon + '.json';
-//   fs.readFile(filePath, function(err, data) {
-//     res.send(data);
-//   });
-// });
-
-// app.get('/', function(req, res){
-//   res.sendfile('templates/index.html');
-// });
-
-// app.post('/', function(req, res){
-
-//   var data = querystring.stringify({
-//         uuid: req.body.uuid,
-//         major: req.body.major,
-//         minor: req.body.minor,
-//         location: settings.store_id
-//       });
-
-//   var options = {
-//       host: 'localhost',
-//       port: 8000,
-//       path: '/beacon/add/',
-//       method: 'POST',
-//       headers: {
-//           'Content-Type': 'application/x-www-form-urlencoded',
-//           'Content-Length': Buffer.byteLength(data)
-//       }
-//   };
-
-//   var req = http.request(options, function(res) {
-//       res.setEncoding('utf8');
-//       res.on('data', function (chunk) {
-//           console.log('body: ' + chunk);
-//       });
-//   });
-
-//   req.write(data);
-//   req.end();
-
-//   res.sendfile('templates/index.html');
-
-// });
-
 router.get('/list', function(req, res) {
     Beacon.pings.find({}).toArray(function(err, docs) {
         res.json({
