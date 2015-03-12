f1fd826cff27d258214aade6ce01013cc1606a3c
goliatone <burgosemi@gmail.com>
2015-03-12 17:05:28 -0400

    ROUTES: demo.js adding user register

--- routes/demo.js
+++ routes/demo.js
@@ -2,28 +2,43 @@ var fs = require('fs');
 var express = require('express');
 var router = express.Router();
 
-router.post('/', function(req, res) {
-    fs.readFile(req.files.image.path, function (err, data) {
+router.post('/register', function(req, res) {
 
-        var imageName = req.files.image.name;
+    var uuid = req.body.uuid; //Device UUID for now, using iPhone app
+    var email = req.body.email;
+
+    console.log('REGISTERING USER', email);
+    console.log('REGISTERING IMAGE', req.files);
+    var image = req.files['image[]'];
+    fs.readFile(image.path, function (err, data) {
+
+        var imageName = image.name;
 
         /// If there's an error
         if(!imageName){
-            console.log("There was an error")
-            res.redirect("/");
-            res.end();
+            console.log("There was an error");
+            res.send({status:false, message:'Image name not present'});
         } else {
 
           var newPath = process.env.PWD + "/public/images/avatars/" + imageName;
+          var imageUrl = "/user/avatar/" + imageName;
 
           /// write file to uploads/fullsize folder
           fs.writeFile(newPath, data, function (err) {
-            /// let's see it
-            res.redirect("/uploads/avatar/" + imageName);
+                //We should check for ajax in headers:
+                res.send({status: true, result: {
+                    url: imageUrl,
+                    email: email,
+                    uuid: uuid,
+                    createdAt: Date.now()
+                }
+            });
+            // res.redirect(imageUrl);
           });
         }
     });
 });
+
 router.get('/avatar/:file', function (req, res){
     console.log('ERE');
     // return res.send({msg:process.env.PWD + "/public/images/avatars/" + req.params.file})
@@ -42,5 +57,5 @@ router.get('/avatar/:file', function (req, res){
 
 module.exports = function(app){
     //TODO: namespace it to demo?!
-    app.use('/upload', router);
+    app.use('/user', router);
 };
