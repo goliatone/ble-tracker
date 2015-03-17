28781ad5b7296f04899b483ae75726231b0e9baf
goliatone <burgosemi@gmail.com>
2015-03-17 13:11:17 -0400

    WIDGET userside show temp data

--- public/js/app/widgets/userside/userside.js
+++ public/js/app/widgets/userside/userside.js
@@ -9,7 +9,11 @@ define('userside', function(require){
         template: template,
         append:true,
         data:{
-            members:[]
+            members:[
+                // {id:'95a178046cb015014e0ce3927bac870b'},
+                // {id:'270faa15aea3467ea7688e69d728aea7'},
+                // {id:'b2f3a868a97a60f444e8a7b6a8c16a8a'}
+            ]
         }
 
     });
