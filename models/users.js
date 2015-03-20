ab08af69cbfb5bcca937a8880f39f5e89be25190
goliatone <burgosemi@gmail.com>
2015-03-20 15:36:13 -0400

    MODEL: user updated

--- models/users.js
+++ models/users.js
@@ -7,13 +7,15 @@ function User(){}
 User.collection = db.collection('users');
 
 
-User.savePing = function(payload){
-    User.collection.insert([payload]);
+User.savePing = function(payload, cb){
+    User.collection.insert([payload], cb);
 };
 
-User.saveConfig = function(payload){
-    User.collection.insert([payload]);
+User.saveConfig = function(payload, cb){
+    User.collection.insert([payload], cb);
 };
+
+// User.createUser = function()
 
 
 module.exports = User;
