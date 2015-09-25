da9a9da6e8366b321e9208936cd54dc978acad4f
goliatone <burgosemi@gmail.com>
2015-09-25 13:35:13 -0400

    ROUTES: messages updated, added SNS

--- routes/messages.js
+++ routes/messages.js
@@ -2,9 +2,25 @@ var express = require('express'),
     extend = require('gextend'),
     router = express();
 
-var sns = require('node-sns');
+var SNS = require('node-sns');
 
-router.get('/dashboard', function(req, res) {
+
+var SCECRET_ACCESS_KEY = process.env.SCECRET_ACCESS_KEY,
+    ACCESS_KEY = process.env.ACCESS_KEY,
+    APPLICATION_ARN = process.env.APPLICATION_ARN,
+    ENDPONT_ARN = process.env.ENDPONT_ARN;
+
+var client = new SNS({
+    region: 'us-east-1',
+    apiVersion: '2010-03-31',
+    key: ACCESS_KEY,
+    secret: SCECRET_ACCESS_KEY,
+    platform: 'APNS_SANDBOX',
+    applicationArn: APPLICATION_ARN
+});
+
+
+router.get('/', function(req, res) {
     res.sendFile(process.env.PWD + '/public/messages.html');
 });
 
@@ -12,20 +28,28 @@ router.get('/dashboard', function(req, res) {
 router.post('/send', function(req, res) {
     var payload =  req.body;
     // var payload =  extend({}, req.body);
+/*
+    findEndpointArn(req.body.receiver).then(function(arn){
+        return client.publish(arn, req.body.message);
+    }).then(function(){
 
-    payload.uuid = req.params.uuid;
+    }).catch(function(e){
+
+    });
+*/
+    if(req.body.message){
+        client.publish(ENDPONT_ARN, req.body.message, function(){
+            console.log('HERE', arguments);
+        });
+    }
+
+    console.log('BODY', payload);
 
     res.json({
         success: true,
         response: "Message sent"
     });
 });
-
-
-
-
-
-
 
 module.exports = function(server) {
     //register beacons with ble path prefix.
